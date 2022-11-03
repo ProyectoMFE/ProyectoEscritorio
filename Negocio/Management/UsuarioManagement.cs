9c236cae3bb58fc9515ad81428323d43c51b5c70
Fco Javier Fraga Hernández <87396998+javierfh03@users.noreply.github.com>
2022-11-03 20:06:47 +0100

    Management de usuario

--- Negocio/Management/UsuarioManagement.cs
+++ Negocio/Management/UsuarioManagement.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Datos.DAO;
 using Datos.Infrastructure;
 using Negocio.EntitiesDTO;
@@ -13,18 +9,14 @@ namespace Negocio.Management
     {
         public UsuarioDTO obtenerUsuario(string correo)
         {
-            USUARIOS usu = new UsuarioDAO().buscar(correo);
-            UsuarioDTO usu1 = new UsuarioDTO();
+            USUARIOS usuOld = new UsuarioDAO().buscar(correo);
+            UsuarioDTO usuario = new UsuarioDTO();
 
-            usu1.idUsuario = usu.ID_USUARIO;
-            usu1.correo = usu.CORREO;
-            usu1.contrasenia = usu.CONTRASENIA;
-            usu1.nombre = usu.NOMBRE;
-            usu1.primerApellido = usu.PRIMER_APELLIDO;
-            usu1.segundoApellido = usu.SEGUNDO_APELLIDO;
-            usu1.tipo = usu.TIPO;
+            //Utils.parse(usuOld, ref usuario);
+            
+            parse(usuOld, usuario);
 
-            return usu1;
+            return usuario;
         }
 
         public List<UsuarioDTO> obtenerUsuarios()
@@ -35,19 +27,18 @@ namespace Negocio.Management
             foreach (USUARIOS usuOld in new UsuarioDAO().consultar())
             {
                 usuario = new UsuarioDTO();
-
+                //Utils.parse(usuOld, ref usuario);
                 usuario.idUsuario = usuOld.ID_USUARIO;
-                usuario.correo = usuOld.CORREO;
-                usuario.nombre = usuOld.NOMBRE;
-                usuario.primerApellido = usuOld.PRIMER_APELLIDO;
-                usuario.segundoApellido = usuOld.SEGUNDO_APELLIDO;
-                usuario.contrasenia = usuOld.CONTRASENIA;
-                usuario.tipo = usuOld.TIPO;
 
                 usuarios.Add(usuario);
             }
 
             return usuarios;
+        }
+
+        private void parse(USUARIOS usuOld, UsuarioDTO usuario)
+        {
+            usuario.idUsuario = usuOld.ID_USUARIO;
         }
     }
 }
---
+++ Negocio/Utils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    internal class Utils
+    {
+        public static void parse<T>(object sourceObject, ref T destObject)
+        {
+            // if eiter the source, or destination is null, return
+            if (sourceObject == null || destObject == null)
+                return;
+
+            // Get the type of each object
+            Type sourceType = sourceObject.GetType();
+            Type targetType = destObject.GetType();
+
+            // Loop through the source properties
+            foreach (PropertyInfo p in sourceType.GetProperties())
+            {
+                // Get the matching property in the destination object
+                PropertyInfo targetObj = targetType.GetProperty(p.Name);
+                // If there is none, skip
+                
+                if (targetObj == null)
+                    continue;
+
+                // Set the value in the destination
+                targetObj.SetValue(destObject, p.GetValue(sourceObject, null), null);
+            }
+        }
+    }
+}
