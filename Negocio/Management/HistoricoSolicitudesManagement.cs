1b64de9458c2d80e0a0c0596de6f3888c3609451
Fco Javier Fraga Hernández <pachi030919@gmail.com>
2022-11-09 11:25:29 +0100

    Management "Acabados"

---
+++ Negocio/Management/HistoricoSolicitudesManagement.cs
@@ -0,0 +1,39 @@
+﻿using Datos.DAO;
+using Datos.Infrastructure;
+using Negocio.EntitiesDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Management
+{
+    public class HistoricoSolicitudesManagement
+    {
+        public List<HistoricoSolicitudesDTO> listarSolicitudes()
+        {
+            List<HistoricoSolicitudesDTO> solicitudes = new List<HistoricoSolicitudesDTO>();
+            HistoricoSolicitudesDTO solicitud;
+
+            foreach (HISTORICO_SOLICITUDES solOld in new HistoricoSolicitudesDAO().Consultar())
+            {
+                solicitud = new HistoricoSolicitudesDTO();
+
+                ParseNew(solOld, solicitud);
+
+                solicitudes.Add(solicitud);
+            }
+
+            return solicitudes;
+        }
+
+        private void ParseNew(HISTORICO_SOLICITUDES solOld, HistoricoSolicitudesDTO solNew)
+        {
+            solNew.numSerie = solOld.NUM_SERIE;
+            solNew.idUsuario = solOld.ID_USUARIO;
+            solNew.ultimatum = solOld.ULTIMATUM;
+            solNew.fecha = solOld.FECHA;
+        }
+    }
+}
--- Negocio/Management/SolicitudManagement.cs
+++ Negocio/Management/SolicitudManagement.cs
@@ -13,7 +13,46 @@ namespace Negocio.Management
     {
         public bool insertarSolicitud(string correo, string numSerie)
         {
-            return 
+            return new SolicitudDAO().insertar(correo, numSerie);
+        }
+
+        public bool finalizarSolicitud(string correo, string numSerie)
+        {
+            return new SolicitudDAO().finalizar(correo, numSerie);
+        }
+
+        public bool aceptarSolicitud(string correo, string numSerie)
+        {
+            return new SolicitudDAO().aceptar(correo, numSerie);
+        }
+
+        public bool rechazarSolicitud(string correo, string numSerie)
+        {
+            return new SolicitudDAO().rechazar(correo, numSerie);
+        }
+
+        public List<SolicitudDTO> listarSolicitudes()
+        {
+            List<SolicitudDTO> solicitudes = new List<SolicitudDTO>();
+            SolicitudDTO solicitud;
+
+            foreach (SOLICITUDES solOld in new SolicitudDAO().Consultar())
+            {
+                solicitud = new SolicitudDTO();
+
+                ParseNew(solOld, solicitud);
+
+                solicitudes.Add(solicitud);
+            }
+
+            return solicitudes;
+        }
+
+        private void ParseNew(SOLICITUDES solOld, SolicitudDTO solNew)
+        {
+            solNew.numSerie = solOld.NUM_SERIE;
+            solNew.idUsuario = solOld.ID_USUARIO;
+            solNew.estado = solOld.ESTADO;
         }
     }
 }
