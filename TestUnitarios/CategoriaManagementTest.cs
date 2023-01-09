d912cf23c6433d364188ef4ccacc3e3a44faf2a1
Fco Javier Fraga Hernández <87396998+javierfh03@users.noreply.github.com>
2023-01-09 21:21:10 +0100

    Test categoria

---
+++ TestUnitarios/CategoriaManagementTest.cs
@@ -0,0 +1,71 @@
+﻿using Negocio.EntitiesDTO;
+using Negocio.Management;
+
+namespace TestUnitarios
+{
+    [TestClass]
+    public class CategoriaManagementTest
+    {
+        private Categoria InicializarCategoria()
+        {
+            Categoria test = new Categoria();
+            test.idCategoria = 1488;
+            test.nombre = "Prueba";
+
+            return test;
+        }
+
+        [TestMethod]
+        public void TestInsertarTrue()
+        {
+            CategoriaManagement m = new CategoriaManagement();
+            Categoria test = InicializarCategoria();
+
+            Assert.AreEqual(true, m.InsertarCategoria(test));
+        }
+
+        [TestMethod]
+        public void TestInsertarFalse()
+        {
+            CategoriaManagement m = new CategoriaManagement();
+            Categoria test = InicializarCategoria();
+
+            Assert.AreEqual(false, m.InsertarCategoria(test));
+        }
+
+        [TestMethod]
+        public void TestConsultarTrue()
+        {
+            CategoriaManagement m = new CategoriaManagement();
+            Categoria test = InicializarCategoria();
+            
+            Assert.AreNotEqual(null, m.ObtenerCategoria(test.idCategoria));
+        }
+
+        [TestMethod]
+        public void TestConsultarFalse()
+        {
+            CategoriaManagement m = new CategoriaManagement();
+
+            Assert.AreEqual(null, m.ObtenerCategoria(int.MaxValue));
+        }
+
+        [TestMethod]
+        public void TestBorrarTrue()
+        {
+            CategoriaManagement m = new CategoriaManagement();
+            Categoria test = InicializarCategoria();
+
+            Assert.AreEqual(true, m.BorrarCategoria(test.idCategoria));
+        }
+
+        [TestMethod]
+        public void TestBorrarFalse()
+        {
+            CategoriaManagement m = new CategoriaManagement();
+            Categoria test = InicializarCategoria();
+
+            Assert.AreEqual(false, m.BorrarCategoria(test.idCategoria));
+        }
+    }
+}
