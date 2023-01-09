2e698dca029c0d23685d0d9c223b7f7a9df02ada
Fco Javier Fraga Hernández <87396998+javierfh03@users.noreply.github.com>
2023-01-09 19:57:40 +0100

    Primeras pruebas

--- TestUnitarios/UsuarioManagementTest.cs
+++ TestUnitarios/UsuarioManagementTest.cs
@@ -1,15 +1,70 @@
 using Negocio.Management;
 using Negocio.EntitiesDTO;
+using System.Text.Json;
 
 namespace TestUnitarios
 {
     [TestClass]
     public class UsuarioManagementTest
     {
-        [TestMethod]
-        public void TestLogin()
+        private Usuario InicializarUsuario()
         {
-            bool insertado = new UsuarioManagement().InsertarUsuario(new Usuario());
+            Usuario test = new Usuario();
+            test.correo = "test@test.com";
+            test.nombre = "Jhon";
+            test.primerApellido = "Doe";
+            test.segundoApellido = "";
+            test.tipo = "P";
+            test.contrasenia = "098f6bcd4621d373cade4e832627b4f6"; // test
+
+            return test;
+        }
+
+        [TestMethod]
+        public void TestInsertarTrue()
+        {
+            UsuarioManagement um = new UsuarioManagement();
+            Usuario test = InicializarUsuario();
+
+            Assert.AreEqual(true, um.InsertarUsuario(test));
+        }
+
+       [TestMethod]
+        public void TestInsertarFalse()
+        {
+            UsuarioManagement um = new UsuarioManagement();
+            Usuario test = InicializarUsuario();
+
+            Assert.AreEqual(false, um.InsertarUsuario(test));
+        }
+
+        [TestMethod]
+        public void TestBorrarTrue()
+        {
+            UsuarioManagement um = new UsuarioManagement();
+            Usuario test = InicializarUsuario();
+
+            Assert.AreEqual(true, um.BorrarUsuario(test.correo));
+        }
+
+        [TestMethod]
+        public void TestBorrarFalse()
+        {
+            UsuarioManagement um = new UsuarioManagement();
+            Usuario test = InicializarUsuario();
+
+            Assert.AreEqual(false, um.BorrarUsuario(test.correo));
+        }
+
+        [TestMethod]
+        public void TestModificarTrue()
+        {
+            UsuarioManagement um = new UsuarioManagement();
+            Usuario test = InicializarUsuario();
+
+            test.nombre = "Willyrex";
+
+            Assert.AreEqual(true, um.ModificarUsuario(test));
         }
     }
 }
